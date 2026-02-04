d32ad9a4f2e9b931e51f3a6f73aff2f996d420d1
Hafeezullah2343 <hafeeezullah2006@gmail.com>
2026-02-04 19:20:53 +0500

    Logest Common Prefix

---
+++ src/LongestCommonPrefix.java
@@ -0,0 +1,28 @@
+public class LongestCommonPrefix {
+
+    public static String longestCommonPrefix(String[] strs) {
+
+        if (strs == null || strs.length == 0) return "";
+
+        String prefix = strs[0];
+
+        for (int i = 1; i < prefix.length(); i++) {
+
+            char ch = prefix.charAt(i);
+
+            for(int j = 0; j < strs.length; j++){
+                if(i >= strs[j].length() || strs[j].charAt(i) != ch){
+                    return prefix.substring(0, i);
+                }
+            }
+        }
+        return prefix;
+    }
+
+    public static void main(String[] args) {
+
+       String[] strs = {"flower","flow","flight"};
+       System.out.println(longestCommonPrefix(strs));
+
+    }
+}
